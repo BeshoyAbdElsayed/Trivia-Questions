7059cbef84754e456b3bf4ca3f5e852ae252c069
Beshoy Abd Elsayed <beshoabdo8@gmail.com>
2021-03-08 16:48:32 +0200

    create tests for GET categories, questions, questionsByCategory

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}@{}/{}".format('postgres:password', 'localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}@{}/{}".format('postgres:password', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,8 +33,58 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
-    test_
+    # helper functions
+    def assertError(self, res, body, code, message):
+        self.assertEqual(res.status_code, code)
+        self.assertFalse(body['success'])
+        self.assertEqual(body['error'], code)
+        self.assertEqual(body['message'], message)
 
+
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        body = res.get_json()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(body['categories'])
+
+    def test_get_all_questions_success(self):
+        res = self.client().get('/questions')
+        body = res.get_json()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(body['success'])
+        self.assertTrue(body['questions'])
+        self.assertTrue(body['total_questions'])
+        
+    def test_get_all_questions_fail(self):
+        res = self.client().get('/questions?page=1000')
+        body = res.get_json()
+
+        self.assertError(res, body, 404, 'resource not found')
+    
+    def test_get_questions_by_category_success(self):
+        res = self.client().get('/categories/1/questions')
+        body = res.get_json()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(body['success'])
+        self.assertTrue(body['questions'])
+        self.assertTrue(body['total_questions'])
+        self.assertEqual(body['current_category'], 1)
+
+    def test_get_questions_by_category_fail(self):
+        res = self.client().get('/categories/1/questions?page=1000')
+        body = res.get_json()
+
+        self.assertError(res, body, 404, 'resource not found')
+
+        res = self.client().get('/categories/1000/questions')
+        body = res.get_json()
+
+        self.assertError(res, body, 422, 'unprocessable')
+        
+        
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
